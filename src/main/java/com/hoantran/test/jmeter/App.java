b96c4bb8c4b975d1d568bbb01f74cd0f27a48277
Kalaiyarasan Perumal <kalaiyarasan.perumal.-nd@disney.com>
2024-10-02 22:55:55 +0530

    Test commit 2

--- src/main/java/com/hoantran/test/jmeter/App.java
+++ src/main/java/com/hoantran/test/jmeter/App.java
@@ -1,7 +1,7 @@
-package com.hoantran.test.jmeter;
+package src.main.java.com.hoantran.test.jmeter;
 
-import java.io.File;
-import java.io.IOException;
+// import java.io.File;
+// import java.io.IOException;
 
 /**
  * Hello world!
@@ -10,33 +10,35 @@ import java.io.IOException;
 public class App 
 {
     public static void main(String[] args) {
+        System.out.println("gdcvjhdsbch");
         // Define the command to execute
-        String[] command = {
-            "jmeter", 
-            "-n", 
-            "-t", 
-            "/Users/kalaiyarasan/Documents/JMETER/jmeter-webload-example-master/src/test/jmeter/page-load.jmx", 
-            "-l", 
-            "results.jtl"
-        };
+        // String[] command = {
+        //     "jmeter", 
+        //     "-n", 
+        //     "-t", 
+        //     "/Users/kalaiyarasan/Documents/JMETER/jmeter-webload-example-master/src/test/jmeter/page-load.jmx", 
+        //     "-l", 
+        //     "results.jtl"
+        // };
 
-        // Create a ProcessBuilder
-        ProcessBuilder processBuilder = new ProcessBuilder(command);
+        // // Create a ProcessBuilder
+        // ProcessBuilder processBuilder = new ProcessBuilder(command);
         
-        // Set the working directory
-        processBuilder.directory(new File("/Users/kalaiyarasan/Documents/JMETER/jmeter-webload-example-master"));
+        // // Set the working directory
+        // processBuilder.directory(new File("/Users/kalaiyarasan/Documents/JMETER/jmeter-webload-example-master"));
 
-        try {
-            // Start the process
-            Process process = processBuilder.start();
+        // try {
+        //     // Start the process
+        //     Process process = processBuilder.start();
 
-            // Wait for the process to complete
-            int exitCode = process.waitFor();
-            System.out.println("Process finished with exit code: " + exitCode);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        //     // Wait for the process to complete
+        //     int exitCode = process.waitFor();
+        //     System.out.println("Process finished with exit code: " + exitCode);
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // } catch (InterruptedException e) {
+        //     e.printStackTrace();
+        // }
+       
     }
 }
